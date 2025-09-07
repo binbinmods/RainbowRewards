e19da9abff82b6adaf7361d0d165f31dd78e8a0a
binbinmods <binbinmods@gmail.com>
2025-09-06 22:10:10 -0400

    Initial setup

---
+++ RainbowRewardsFunctions.cs
@@ -0,0 +1,85 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+// using Obeliskial_Content;
+// using Obeliskial_Essentials;
+using System.IO;
+using static UnityEngine.Mathf;
+using UnityEngine.TextCore.LowLevel;
+using static RainbowRewards.Plugin;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace RainbowRewards
+{
+    public class RainbowRewardsFunctions
+    {
+        public static CardData GetRandomCardByClass(Enums.CardClass cardClass, TierRewardData tierReward, string[] alreadyExistingCards)
+        {
+            CardData _cardData = null;
+            List<string> stringList1 = new List<string>(Globals.Instance.CardListNotUpgradedByClass[cardClass]);
+            int num10 = UnityEngine.Random.Range(0, 100);
+            bool flag2 = true;
+            while (flag2)
+            {
+                flag2 = false;
+                bool flag3 = false;
+                while (!flag3)
+                {
+                    flag2 = false;
+                    _cardData = Globals.Instance.GetCardData(stringList1[UnityEngine.Random.Range(0, stringList1.Count)], false);
+                    if (!flag2)
+                    {
+                        if (num10 < tierReward.Common)
+                        {
+                            if (_cardData.CardRarity == Enums.CardRarity.Common)
+                                flag3 = true;
+                        }
+                        else if (num10 < tierReward.Common + tierReward.Uncommon)
+                        {
+                            if (_cardData.CardRarity == Enums.CardRarity.Uncommon)
+                                flag3 = true;
+                        }
+                        else if (num10 < tierReward.Common + tierReward.Uncommon + tierReward.Rare)
+                        {
+                            if (_cardData.CardRarity == Enums.CardRarity.Rare)
+                                flag3 = true;
+                        }
+                        else if (num10 < tierReward.Common + tierReward.Uncommon + tierReward.Rare + tierReward.Epic)
+                        {
+                            if (_cardData.CardRarity == Enums.CardRarity.Epic)
+                                flag3 = true;
+                        }
+                        else if (_cardData.CardRarity == Enums.CardRarity.Mythic)
+                            flag3 = true;
+                    }
+                }
+                int rarity = UnityEngine.Random.Range(0, 100);
+                string id = _cardData.Id;
+                _cardData = Globals.Instance.GetCardData(Functions.GetCardByRarity(rarity, _cardData), false);
+                if ((UnityEngine.Object)_cardData == (UnityEngine.Object)null)
+                {
+                    flag2 = true;
+                }
+                else
+                {
+                    for (int index2 = 0; index2 < alreadyExistingCards.Length; ++index2)
+                    {
+                        if (alreadyExistingCards[index2] == _cardData.Id)
+                        {
+                            flag2 = true;
+                            break;
+                        }
+                    }
+                }
+
+            }
+            return _cardData;
+        }
+
+
+
+    }
+}
+
---
+++ RainbowRewardsPatches.cs
@@ -0,0 +1,131 @@
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+// using static Obeliskial_Essentials.Essentials;
+using System;
+using static RainbowRewards.Plugin;
+using static RainbowRewards.CustomFunctions;
+using static RainbowRewards.RainbowRewardsFunctions;
+using System.Collections.Generic;
+using static Functions;
+using UnityEngine;
+// using Photon.Pun;
+using TMPro;
+using System.Linq;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Diagnostics;
+// using Unity.TextMeshPro;
+
+// Make sure your namespace is the same everywhere
+namespace RainbowRewards
+{
+
+    [HarmonyPatch] // DO NOT REMOVE/CHANGE - This tells your plugin that this is part of the mod
+
+    public class RainbowRewardsPatches
+    {
+        public static bool devMode = false; //DevMode.Value;
+        public static bool bSelectingPerk = false;
+        public static bool IsHost()
+        {
+            return GameManager.Instance.IsMultiplayer() && NetworkManager.Instance.IsMaster();
+        }
+
+
+
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(RewardsManager), "SetRewards")]
+        public static void SetRewardsPrefix(ref int ___numCardsReward)
+        {
+            if (IncreaseCardsTo4.Value)
+            {
+                ___numCardsReward = 4;
+
+            }
+
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(RewardsManager), "ShowRewards")]
+        public static void ShowRewardsPrefix(ref Dictionary<int, string[]> ___cardsByOrder,
+            int ___numCardsReward,
+            Hero[] ___theTeam,
+            TierRewardData ___tierReward,
+            TierRewardData ___tierRewardBase,
+            TierRewardData ___tierRewardInf)
+        {
+            CardData _cardData = null;
+
+            foreach (int key in ___cardsByOrder.Keys)
+            {
+                if (___cardsByOrder[key] == null || ___cardsByOrder[key].Length == 0)
+                {
+                    continue;
+                }
+                UnityEngine.Random.InitState((AtOManager.Instance.currentMapNode + key + AtOManager.Instance.GetGameId()).GetDeterministicHashCode());
+                // int randInt = UnityEngine.Random.Range(0, 100); 
+                Hero hero = ___theTeam[key];
+                int length = ___numCardsReward;
+                string[] arr = new string[length];
+                Dictionary<Enums.CardClass, List<string>> dictionary = Globals.Instance.CardListNotUpgradedByClass;
+
+                for (int index1 = 0; index1 < length; ++index1)
+                {
+                    _cardData = GetRandomCardByClass((Enums.CardClass)index1, ___tierReward, arr);
+                    arr[index1] = _cardData.Id;
+                }
+                ___cardsByOrder[key] = Functions.ShuffleArray<string>(arr);
+
+            }
+
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SteamManager), "SetObeliskScore")]
+        public static bool SetObeliskScorePrefix(ref SteamManager __instance, int score, bool singleplayer = true)
+        {
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SteamManager), "SetScore")]
+        public static bool SetScorePrefix(ref SteamManager __instance, int score, bool singleplayer = true)
+        {
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SteamManager), "SetSingularityScore")]
+        public static bool SetSingularityScorePrefix(ref SteamManager __instance, int score, bool singleplayer = true)
+        {
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SteamManager), "SetObeliskScoreLeaderboard")]
+        public static bool SetObeliskScoreLeaderboardPrefix(ref SteamManager __instance, int score, bool singleplayer = true)
+        {
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SteamManager), "SetScoreLeaderboard")]
+        public static bool SetScoreLeaderboardPrefix(ref SteamManager __instance, int score, bool singleplayer = true)
+        {
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SteamManager), "SetSingularityScoreLeaderboard")]
+        public static bool SetSingularityScoreLeaderboardPrefix(ref SteamManager __instance, int score, bool singleplayer = true)
+        {
+            return false;
+        }
+
+
+    }
+}
